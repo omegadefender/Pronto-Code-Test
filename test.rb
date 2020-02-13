e7c726a20349be20dcc80b5ce3c1fb9dbfa75630
AirrQu0tes <hastings.james.m@gmail.com>
2020-02-13 14:44:35 +1100

    user input added

--- test.rb
+++ test.rb
@@ -1,1 +1,13 @@
 puts "This is a coding test for ProntoWoven"
+puts "Input Robot Direction as a string/F = forward/B = Backwards/R = turn right 90 degrees/ L = turn left 90 degrees"
+puts "Follow each character by a number of units eg. B2 means move back two units, R1 means turn right 90 degrees once"
+puts "Seperate each instruction by a comma, eg `F1,R1,B2,L1,B3`"
+puts "Enter your movement instructions and the program will tell you the shortest distance to get back home"
+
+def prompt(*args)
+    print(*args)
+    gets
+end
+
+movement = prompt "Movement: "
+print movement
