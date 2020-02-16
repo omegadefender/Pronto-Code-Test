9bb47bb0df77d3cde4cb6b2b8978b2435e232e91
AirrQu0tes <hastings.james.m@gmail.com>
2020-02-16 22:58:46 +1100

    direction changes added to code

--- test.rb
+++ test.rb
@@ -1,4 +1,4 @@
-puts "Input Robot Direction as a string\nF = forward\nB = Backwards\nR = turn right 90 degrees\nL = turn left 90 degrees"
+puts "Give commands to the ROBOT\nF = forward\nB = Backwards\nR = turn right 90 degrees\nL = turn left 90 degrees"
 puts "Follow each character by a number of units eg. B1 means move back one unit, R2 means turn right 90 degrees twice"
 puts "Seperate each instruction by a comma, eg. `F1,R1,B2,L1,B3`"
 puts "Enter your movement instructions and the program will tell you the shortest distance to get back home"
@@ -20,29 +20,48 @@ end
 # Sets the final location of Robot, after Commands have been processed
 def location
     moves = movementArr()
-    horArr = [-5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5]
-    verArr = [-5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5]
     facing = 'North'
     facingArr = ['North', 'East', 'South', 'West']
+    facingHash = Hash[facingArr.map.with_index.to_a]
     locHash = { :vert => 0, :hor => 0 }
 
     moves.each do |e|
-        if e[0] == "F"
+        if e[0] == "F" && facing == 'North'
             units = e[1].to_i
             locHash[:vert] = locHash[:vert] + units
-            puts locHash            
-        elsif e[0] == "B"
-            units = e[1].to_i
-            locHash[:vert] = locHash[:vert] - units
-            puts locHash
-        elsif e[0] == "R"
+        elsif e[0] == "F" && facing == 'East'
             units = e[1].to_i
             locHash[:hor] = locHash[:hor] + units
-            puts locHash
-        elsif e[0] == "L"
+        elsif e[0] == "F" && facing == 'South'
+            units = e[1].to_i
+            locHash[:vert] = locHash[:vert] - units
+        elsif e[0] == "F" && facing == 'West'
             units = e[1].to_i
             locHash[:hor] = locHash[:hor] - units
-            puts locHash
+        elsif e[0] == "B" && facing == 'North'
+            units = e[1].to_i
+            locHash[:vert] = locHash[:vert] - units
+        elsif e[0] == "B" && facing == 'East'
+            units = e[1].to_i
+            locHash[:hor] = locHash[:hor] - units
+        elsif e[0] == "B" && facing == 'South'
+            units = e[1].to_i
+            locHash[:vert] = locHash[:vert] + units
+        elsif e[0] == "B" && facing == 'West'
+            units = e[1].to_i
+            locHash[:hor] = locHash[:hor] + units
+        elsif e[0] == "R"
+            units = e[1].to_i
+            indNum = facingHash[facing]
+            indNum = indNum + units
+            facing = facingArr[indNum]
+            puts facing
+        elsif e[0] == "L"
+            units = e[1].to_i
+            indNum = facingHash[facing]
+            indNum = indNum - units
+            facing = facingArr[indNum]
+            puts facing
         end
     end
 end
