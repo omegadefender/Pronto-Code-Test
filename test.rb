e9e48150a61eb3150156e86eda111c2ef54f2164
AirrQu0tes <hastings.james.m@gmail.com>
2020-02-16 19:49:53 +1100

    basic movement established

--- test.rb
+++ test.rb
@@ -1,5 +1,5 @@
 puts "Input Robot Direction as a string\nF = forward\nB = Backwards\nR = turn right 90 degrees\nL = turn left 90 degrees"
-puts "Follow each character by a number of units eg. B2 means move back two units, R1 means turn right 90 degrees once"
+puts "Follow each character by a number of units eg. B1 means move back one unit, R2 means turn right 90 degrees twice"
 puts "Seperate each instruction by a comma, eg. `F1,R1,B2,L1,B3`"
 puts "Enter your movement instructions and the program will tell you the shortest distance to get back home"
 
@@ -17,19 +17,29 @@ end
 
 def location
     moves = movementArr()
-    horArr = [0, 1, 2, 3]
-    verArr = [0, 1, 2, 3]
-    locHash = { vert: 0, hor: 0 }
+    horArr = [-5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5]
+    verArr = [-5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5]
+    facing = 'North'
+    facingArr = ['North', 'East', 'South', 'West']
+    locHash = { :vert => 0, :hor => 0 }
 
     moves.each do |e|
         if e[0] == "F"
-            puts e[0]
+            units = e[1].to_i
+            locHash[:vert] = locHash[:vert] + units
+            puts locHash            
         elsif e[0] == "B"
-            puts e[0]
+            units = e[1].to_i
+            locHash[:vert] = locHash[:vert] - units
+            puts locHash
         elsif e[0] == "R"
-            puts e[0]
+            units = e[1].to_i
+            locHash[:hor] = locHash[:hor] + units
+            puts locHash
         elsif e[0] == "L"
-            puts e[0]
+            units = e[1].to_i
+            locHash[:hor] = locHash[:hor] - units
+            puts locHash
         end
     end
 end
